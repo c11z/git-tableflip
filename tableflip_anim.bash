7067322327618513d8d8e33c7b19596681805bcf
dhood <dhood@users.noreply.github.com>
2018-05-16 18:59:53 -0400

    Add tableflip animation

---
+++ tableflip_anim.bash
@@ -0,0 +1,10 @@
+echo -en "(゜-゜)       ┬─┬﻿" "\r"
+sleep 0.5
+echo -en "   (゜-゜)    ┬─┬﻿" "\r"
+sleep 0.5
+echo -en "      (゜-゜) ┬─┬﻿" "\r"
+sleep 1
+echo -en "     (\゜-゜)\┬─┬﻿" "\r"
+sleep 1
+echo "      ( ╯°□° )╯ ┻━┻"
+sleep 1
